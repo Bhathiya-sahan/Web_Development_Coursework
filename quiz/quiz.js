3ca464df02f0df27f2355e77f35979e06670ad3e
Bhathiya <120437641+bs-thunders@users.noreply.github.com>
2023-04-06 02:52:44 +0530

    Add files via upload

---
+++ quiz/quiz.js
@@ -0,0 +1,75 @@
+let quiz = [
+   {
+      question: "The language spoken by the people by Pakistan is ? ",
+        
+      option: [
+        " Hindi",
+        "Palauan",
+        "Sindhi",
+        "Nauruan"
+      ],
+      answer: 1,
+    },
+    {
+          
+      question: " The World Largest desert is ? ",
+            
+      option: [
+        "Thar",
+        "Kalahari ",
+        "Sahara ",
+        "Sonoran"
+
+      ],
+      answer: 2 ,
+    },
+    {
+            
+      question: "Country that has the highest in Barley Production ? ",
+            
+      option: [
+        "China",
+        " India",
+        "Russia",
+        "France "
+      ],
+      answer: 1,
+    },
+    {
+      question: "The metal whose salts are sensitive to light is ?",
+            
+      option: [
+        "Zinc",
+        "Silver",
+        "Copper",
+        "Aluminium"
+      ],
+      answer: 1,
+    },
+    {
+              
+      question: " The Central Rice Research Station is situated in ?",
+                
+      option: [
+        "Chennai",
+        "Cuttack",
+        "Banglore",
+        "Quilon"
+    
+      ],
+      answer: 2 ,
+    },
+    {
+                
+      question: "Mount Everest is located in ? ",
+                
+      option: [
+        " India ",
+        "Nepal ",
+        "Tibet",
+        " China"
+      ],
+      answer: 1,
+    },
+
+]
